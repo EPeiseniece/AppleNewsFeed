8143d651a5b089a41979e864b0b3a6e5805f63d3
EPeiseniece <87663601+EPeiseniece@users.noreply.github.com>
2021-08-28 13:02:46 +0300

    init

---
+++ AppleNewsFeed/Model/Item.swift
@@ -0,0 +1,51 @@
+//
+//  File.swift
+//  AppleNewsFeed
+//
+//  Created by elina.peiseniece on 27/08/2021.
+//
+
+import UIKit
+import Gloss
+
+class Item: JSONDecodable {
+    
+    var desciption: String
+    var title: String
+    var url: String
+    var urlToImage: String
+    var image: UIImage?
+    var publishedAt: String
+    
+    
+    required init?(json: JSON) {
+        self.desciption = "desciption" <~~ json ?? ""
+        self.title = "title" <~~ json ?? ""
+        self.url = "url" <~~ json ?? ""
+        self.urlToImage = "urlToImage" <~~ json ?? ""
+        self.publishedAt = "publishedAt" <~~ json ?? ""
+        
+        DispatchQueue.main.async {
+            self.image = self.loadImage()
+        }
+    }
+    
+    private func loadImage() -> UIImage? {
+        var returnImage: UIImage?
+        
+        guard let url = URL(string: urlToImage) else {
+            return returnImage
+        }
+        if let data = try? Data(contentsOf: url){
+            if let image = UIImage(data: data){
+                returnImage = image
+            }
+        }
+        return returnImage
+        
+    }
+    
+    
+    
+}
+
